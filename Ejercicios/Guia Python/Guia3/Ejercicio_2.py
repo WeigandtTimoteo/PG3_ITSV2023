a2b6a28739b69e90b16a8a5721dff93f792b9c05
Weigandt Timoteo <weigandttimoteo@gmail.com>
2023-04-14 15:23:32 -0300

    Guia 3, Ejercicio 2

---
+++ Ejercicios/Guia Python/Guia3/Ejercicio_2.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        n1 = int(input("Ingrese el primer numero: "))
+        n2 = int(input("Ingrese el segundo numero: "))
+        print("El resultado de la division es: ", n1 / n2)
+
+    except ZeroDivisionError:
+        print("No se puede dividir por cero")
+
+    finally:
+        print("Fin de la operación")
